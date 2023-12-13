00fdad58fa6224c20f2e055f252fca65ef6e7eb0
Thuta <Thuta@DESKTOP-JLFSII6>
2023-12-13 11:48:15 +0630

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stopwatch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopwatch = new Stopwatch();
+            try
+            {
+                stopwatch.Start();
+                System.Threading.Thread.Sleep(2000);
+                stopwatch.Stop();
+
+                stopwatch.Start();
+                System.Threading.Thread.Sleep(2000);
+                stopwatch.Stop();
+            }
+            catch (InvalidOperationException)
+            {
+
+                Console.WriteLine("Invalid operation");
+            }
+
+        }
+    }
+}
---
+++ Stopwatch.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+
+namespace stopwatch
+{
+    public class Stopwatch
+    {
+        public bool isRunning;
+        public DateTime startTime;
+        public DateTime stopTime;
+
+        public void Start()
+        {
+            if (isRunning)
+            {
+                throw new InvalidOperationException("Stop watch is already starting");
+            }
+            isRunning = true;
+            startTime = DateTime.Now;
+            Console.WriteLine("Stopwatch started");
+        }
+        public void Stop()
+        {
+            if (!isRunning)
+            {
+                throw new InvalidOperationException("Stopwatch is not running");
+            }
+            stopTime = DateTime.Now;
+            isRunning = false;
+            TimeSpan duration = stopTime - startTime;
+            Console.WriteLine("Duration: {0}", duration);
+        }
+    }
+}
